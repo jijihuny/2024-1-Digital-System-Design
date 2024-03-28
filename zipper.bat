aeca4f0f4333dbef8f949bcc58a8d45104315a64
JIJIHUNY <dlwlgns2792@gmail.com>
2024-03-28 12:14:57 +0900

    update script

--- zipper.bat
+++ zipper.bat
@@ -3,4 +3,4 @@
 set SRC_DIR=%1
 set OUTPUT=002_이지훈_12181819_HW_%SRC_DIR%.zip
 
-git archive HEAD -o %OUTPUT% --format=zip --addfile %SRC_DIR%/*.pdf %SRC_DIR%/** ":!**/images/**" ":!**/*.md"
+git archive HEAD -o %OUTPUT% --format=zip --add-file %SRC_DIR%\report.pdf %SRC_DIR%/** ":!**/images/**" ":!**/*.md"
